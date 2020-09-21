2e93193dd6a5e7c45797f407c54fa48c3e56637a
Cooper <eliteswimmer42@gmail.com>
2020-09-21 11:58:27 -0500

    reformatting file structure

--- index.js
+++ index.js
@@ -63,64 +63,15 @@ function readmePrompts() {
 }
 
 
-function generateReadme(answers) {
-    const avatar = `https://github.com/${data.username}.png?size=50`;
-    const gitHub = `https://img.shields.io/badge/Github-${data.username}-4cbbb9`;
+async function getAnswers() {
+  try {
+    const answers = await readmePrompts();
+    generateReadme(answers);
+    writeToFile("README.md", generateReadme(answers));
 
-    return `
-    # ${data.title} 
-    
-    ## Description
-      ${data.description}
-  
-    ## Table of Contents
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Tests](#tests)
-    - [Contributors](#contributors)
-    - [Details](#details)
-      
-    ## Usage
-    Examples of how to use this program: ${data.usage}
-
-    ## Installation
-    Packages required to run this program are: ${data.packages}
-      
-    ## Tests
-    To test, run the following command: ${data.test}
-  
-    ## License
-    ${data.license}
-
-    ## Contributors
-    ${data.contributer}
-  
-    ## Contact
-    \n![Badge](${gitHub}) 
-    \n![Profile Image](${avatar})
-    \nView the project live thanks to GitHub Pages: ${data.url}
-    \nIf you have any questions, contact the author directly at ${data.email}.`;
-}
-
-
-function writeFile(fileName, data) {
-    fs.writeFile(fileName, data, "utf8", function (err) {
-        if (err) {
-            throw err;
-          }
-          console.log("You have successfully written your README file");
-    });
+  } catch (err) {
+  console.log(err);
+  }
 };
 
-
-async function getMovie() {
-    try {
-      const { movie } = await inquirer.prompt({
-        message: "Search a movie:",
-        name: "movie"
-      });
-    } catch (err) {
-    console.log(err);
-    }
-}   
+getAnswers();
